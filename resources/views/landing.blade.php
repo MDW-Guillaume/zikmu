73b736909987ccb50c999fc7f58a6032928408dc
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2023-01-04 09:40:50 +0100

    ADD Music Files

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,9 +5,24 @@
 @endsection
 
 @section('content')
-    <h1>
-        <a href="{{ route('landing') }}">
-            <img src="{{ URL::to('/') }}/img/logo.png" alt="Zik&Mu">
-        </a>
-    </h1>
+        <div class="container login">
+        <h1>
+            <a href="{{ route('landing') }}">
+                <img src="{{ URL::to('/') }}/img/logo.png" alt="Zik&Mu">
+            </a>
+        </h1>
+
+        <h2>Écoutez votre musique favorite en un seul clic</h2>
+
+        <div class="login-method">
+            <div class="signup-method">
+                <span>Profiter gratuitement<br>de toute votre musique</span>
+                <a href="">S'inscrire</a>
+            </div>
+            <div class="signin-method">
+                <span>Vous avez déjà un compte ?</span>
+                <a href="/login">Se connecter</a>
+            </div>
+        </div>
+    </div>
 @endsection
