85940e85d2437ced18340bc4391b72927a39608d
Régis Enguehard <regis@media-management.fr>
2023-05-11 11:39:23 +0200

    reload+add favoris

--- resources/js/reload-in-back.js
+++ resources/js/reload-in-back.js
@@ -18,6 +18,46 @@
 //             });
 //         });
 // });
+function afficheAlbumAvecFavoris() {
+    var form = $('.actionFavorite')
+    for (let i = 0; i < form.length - 1; i++) {
+        $(document).ready(function () {
+            form[i].addEventListener('submit', function (e) {
+                e.preventDefault(); // Empêcher l'envoi par défaut du formulaire
+                console.log(form[i][3])
+                var formData = $(form[i]).serialize(); // Récupérer les données du formulaire
+                console.log(formData)
+                $.ajax({
+                    url: '/favorite',
+                    type: 'POST',
+                    data: formData,
+                    dataType: 'json',
+                    success: function (response) {
+                        if (response.success) {
+                            // Mettre à jour la page sans la recharger
+                            // Par exemple, ajouter le nouveau champ à un tableau existant
+                            if (form[i][3].classList.contains('is-favorite')) {
+                                $(form[i][3]).removeClass('is-favorite')
+                                console.log(document.getElementById('displayMessage'))
+                                document.getElementById('displayMessage').innerHTML = 'La titre a été supprimé de vos Coups de coeur';
+                                document.getElementById('displayMessageContainer').classList.add('show');
+                                setTimeout(function() {document.getElementById('displayMessageContainer').classList.remove('show')}, 4000);
+                            } else {
+                                $(form[i][3]).addClass('is-favorite')
+                                document.getElementById('displayMessage').innerHTML = 'La titre a été ajouté de vos Coups de coeur';
+                                document.getElementById('displayMessageContainer').classList.add('show');
+                                setTimeout(function() {document.getElementById('displayMessageContainer').classList.remove('show')}, 4000);
+                            }
+                        } else {
+                            alert('Une erreur est survenue : ' + response.error);
+                        }
+                    }
+                });
+            });
+        });
+    }
+}
+
 $(document).ready(function () {
     $(document).on('click', 'a', function (event) {
         event.preventDefault();
@@ -28,8 +68,9 @@ $(document).ready(function () {
             type: 'GET',
             success: function (data) {
                 $('#content').html($(data).find('#content').html());
-                reloadScript();
-                index = 0;
+                afficheAlbumAvecFavoris();
+                // reloadScript();
+                // index = 0;
             }
         });
     });
@@ -42,7 +83,7 @@ $(document).ready(function () {
     // });
 });
 
-
+/*
 var scripts = ['unique-song-play.js', 'multiple-song-play.js', 'favorite.js', 'play-album.js', 'audio-event.js', 'song-controller.js', 'waitinglistshow.js'];
 var index = 0;
 var endScript = 0;
@@ -52,9 +93,9 @@ function reloadScript() {
     // Incrémentez l'index et rechargez le script suivant (de manière récursive)
     index++;
     if (index - 1 < scripts.length) {
-        var oldScript = document.querySelector('head script[src="http://[::1]:5173/resources/js/' + scripts[index - 1] + '"]');
+        var oldScript = document.querySelector('head script[src="http://[::1]:5174/resources/js/' + scripts[index - 1] + '"]');
         var newScript = document.createElement('script');
-        newScript.src = 'http://[::1]:5173/resources/js/' + scripts[index - 1];
+        newScript.src = 'http://[::1]:5174/resources/js/' + scripts[index - 1];
         oldScript.parentNode.replaceChild(newScript, oldScript);
 
         reloadScript();
@@ -62,5 +103,5 @@ function reloadScript() {
         endScript++;
     }
 }
-
+*/
 
