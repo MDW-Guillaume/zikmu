2493767930acba4ecd057a438ef6314186022dab
Régis Enguehard <regis@media-management.fr>
2023-05-11 12:03:26 +0200

    upd js

--- resources/js/reload-in-back.js
+++ resources/js/reload-in-back.js
@@ -20,20 +20,74 @@
 // });
 function afficheAlbumAvecFavoris() {
     var form = $('.actionFavorite')
-    for (let i = 0; i < form.length - 1; i++) {
-        $(document).ready(function () {
-            form[i].addEventListener('submit', function (e) {
-                e.preventDefault(); // Empêcher l'envoi par défaut du formulaire
-                console.log(form[i][3])
-                var formData = $(form[i]).serialize(); // Récupérer les données du formulaire
-                console.log(formData)
-                $.ajax({
-                    url: '/favorite',
-                    type: 'POST',
-                    data: formData,
-                    dataType: 'json',
-                    success: function (response) {
-                        if (response.success) {
+    if (form) {
+        for (let i = 0; i < form.length - 1; i++) {
+            $(document).ready(function () {
+                form[i].addEventListener('submit', function (e) {
+                    e.preventDefault(); // Empêcher l'envoi par défaut du formulaire
+                    console.log(form[i][3])
+                    var formData = $(form[i]).serialize(); // Récupérer les données du formulaire
+                    console.log(formData)
+                    $.ajax({
+                        url: '/favorite',
+                        type: 'POST',
+                        data: formData,
+                        dataType: 'json',
+                        success: function (response) {
+                            if (response.success) {
+                                // Mettre à jour la page sans la recharger
+                                // Par exemple, ajouter le nouveau champ à un tableau existant
+                                if (form[i][3].classList.contains('is-favorite')) {
+                                    $(form[i][3]).removeClass('is-favorite')
+                                    console.log(document.getElementById('displayMessage'))
+                                    document.getElementById('displayMessage').innerHTML = 'La titre a été supprimé de vos Coups de coeur';
+                                    document.getElementById('displayMessageContainer').classList.add('show');
+                                    setTimeout(function() {document.getElementById('displayMessageContainer').classList.remove('show')}, 4000);
+                                } else {
+                                    $(form[i][3]).addClass('is-favorite')
+                                    document.getElementById('displayMessage').innerHTML = 'La titre a été ajouté de vos Coups de coeur';
+                                    document.getElementById('displayMessageContainer').classList.add('show');
+                                    setTimeout(function() {document.getElementById('displayMessageContainer').classList.remove('show')}, 4000);
+                                }
+                            } else {
+                                alert('Une erreur est survenue : ' + response.error);
+                            }
+                        }
+                    });
+                });
+            });
+        }
+    }
+}
+
+function favoriteDelete() {
+    var deleteDiv = document.querySelectorAll('.favorite-delete')
+    // var form = $('.actionFavorite')
+
+    for (let i = 0; i < deleteDiv.length; i++) {
+        var lebtn = $(deleteDiv[i]).find('.favorite-button')[0];
+        //  var csrf = $(this).siblings('input[name="csrf"]').val();
+        var csrf = $('input[name="csrf"]').val();
+        $(deleteDiv[i]).on('click', '.favorite-button', function (e) {
+            e.preventDefault(); // Empêcher l'envoi par défaut du formulaire
+
+            var formData = {
+                title: e.target.closest('button').getAttribute('data-id')
+            } // Récupérer les données du formulaire
+
+
+            console.log(formData)
+            $.ajax({
+                url: '/favorite',
+                type: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': csrf
+                },
+                data: formData,
+                dataType: 'json',
+                success: function (response) {
+                    if (response.success) {
+                        if (response.action == 'add') {
                             // Mettre à jour la page sans la recharger
                             // Par exemple, ajouter le nouveau champ à un tableau existant
                             if (form[i][3].classList.contains('is-favorite')) {
@@ -41,18 +95,28 @@ function afficheAlbumAvecFavoris() {
                                 console.log(document.getElementById('displayMessage'))
                                 document.getElementById('displayMessage').innerHTML = 'La titre a été supprimé de vos Coups de coeur';
                                 document.getElementById('displayMessageContainer').classList.add('show');
-                                setTimeout(function() {document.getElementById('displayMessageContainer').classList.remove('show')}, 4000);
+                                setTimeout(function () { document.getElementById('displayMessageContainer').classList.remove('show') }, 4000);
                             } else {
                                 $(form[i][3]).addClass('is-favorite')
                                 document.getElementById('displayMessage').innerHTML = 'La titre a été ajouté de vos Coups de coeur';
                                 document.getElementById('displayMessageContainer').classList.add('show');
-                                setTimeout(function() {document.getElementById('displayMessageContainer').classList.remove('show')}, 4000);
+                                setTimeout(function () { document.getElementById('displayMessageContainer').classList.remove('show') }, 4000);
                             }
                         } else {
-                            alert('Une erreur est survenue : ' + response.error);
+                            var formId = e.target.closest('button').getAttribute('data-id')
+                            var form = $('form[data-id="' + formId + '"]');
+                            form.css('display', 'none');
+                            document.getElementById('displayMessage').innerHTML = 'La titre a été supprimé de vos Coups de coeur';
+                            document.getElementById('displayMessageContainer').classList.add('show');
+                            setTimeout(function () { document.getElementById('displayMessageContainer').classList.remove('show') }, 4000);
                         }
+                    } else {
+                        alert('Une erreur est survenue : ' + response.error);
                     }
-                });
+                },
+                error: function (xhr, status, error) {
+                    // gérer les erreurs de la requête
+                }
             });
         });
     }
@@ -63,12 +127,15 @@ $(document).ready(function () {
         event.preventDefault();
 
         var url = $(this).attr('href');
+
+        console.log('****', url);
         $.ajax({
             url: url,
             type: 'GET',
             success: function (data) {
                 $('#content').html($(data).find('#content').html());
                 afficheAlbumAvecFavoris();
+                favoriteDelete();
                 // reloadScript();
                 // index = 0;
             }
