5c81df44eb2bd2b7c46ba5192fc05b4259dbaa40
Régis Enguehard <regis@media-management.fr>
2023-05-11 14:43:06 +0200

    Revert "@vite ressource"
    This reverts commit f949b2e147e79d16cc2dbc8a1a776c3c3dc8a752.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,10 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        @vite([
-            'resources/scss/app.scss',
-            'resources/scss/mixin.scss'
-        ])
+        @vite(['resources/scss/app.scss'])
+        @vite(['resources/scss/mixin.scss'])
         <title>Zik&Mu - @yield('title')</title>
     </head>
     <body>
--- resources/views/layout/online.blade.php
+++ resources/views/layout/online.blade.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite([
-        'resources/scss/online.scss',
-        'resources/scss/footer.scss',
-        'resources/scss/sidebar.scss',
-        'resources/scss/app.scss',
-        'resources/scss/mixin.scss',
-        'resources/scss/artist.scss',
-        'resources/scss/album.scss',
-        'resources/scss/favorite.scss',
-        'resources/scss/stylepage.scss',
-        'resources/js/reload-in-back.js',
-        'resources/js/multiple-song-play.js',
-        'resources/js/unique-song-play.js',
-        'resources/js/favorite.js',
-        'resources/js/play-album.js',
-        'resources/js/audio-event.js',
-        'resources/js/song-controller.js',
-        'resources/js/waitinglistshow.js'
-    ])
+
+    @vite(['resources/scss/online.scss'])
+    @vite(['resources/scss/footer.scss'])
+    @yield('scss')
+    @vite(['resources/scss/sidebar.scss'])
+    @vite(['resources/scss/app.scss'])
+    @vite(['resources/scss/mixin.scss'])
+    @vite(['resources/js/reload-in-back.js'])
+
+    @vite(['resources/scss/artist.scss'])
+    @vite(['resources/scss/album.scss'])
+    @vite(['resources/scss/favorite.scss'])
+    @vite(['resources/scss/stylepage.scss'])
+    @vite(['resources/js/multiple-song-play.js'])
+    @vite(['resources/js/unique-song-play.js'])
+    @vite(['resources/js/favorite.js'])
+    @vite(['resources/js/play-album.js'])
+    @vite(['resources/js/audio-event.js'])
+    @vite(['resources/js/song-controller.js'])
+    @vite(['resources/js/waitinglistshow.js'])
+
+
     <title>Zik&Mu - @yield('title')</title>
 </head>
 
