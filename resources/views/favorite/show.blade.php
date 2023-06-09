b2ea033d1dd3289d9ed10b139deb3b35eee5dc9f
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2023-06-09 15:17:48 +0200

    FIX style et favoris

--- resources/views/favorite/show.blade.php
+++ resources/views/favorite/show.blade.php
@@ -27,67 +27,79 @@
 
                             {{-- Nom prénom / ou / nom d'utilisateur --}}
                         </p>
-                        <span>Durée : {{ $length }}</span>
-                    </div>
-                    <div class="favorite-actions">
-                        <form action="{{ route('play.fastplayfavorite') }}" method="post" id="playFavoriteBtn">
-                            @csrf
-                                <input type="hidden" value="1" name="position">
-                            <button type="submit" class="play-playlist"><img
-                                    src="{{ URL::to('/img') }}/play.svg" alt=""><span>Lire</span></button>
-
-                        </form>
-                        <form action="{{ route('play.randomplayfavorite') }}" method="post" id="playRandomFavorite">
-                            @csrf
-                            <button type="submit" id="playPlaylistRandom" class="play-playlist-random"><img
-                                    src="{{ URL::to('/img') }}/randomizer.svg"
-                                    alt=""><span>Aléatoire</span></button>
-                        </form>
+                        @if (count($songs) == 0)
                     </div>
                 </div>
             </div>
-
-            <div class="favorite-playlist">
-                @php
-                    $i = 1;
-                @endphp
-                @foreach ($songs as $song)
-                <form action="{{ route('song.uniqueplay') }}" method="post" class="favorite-unique-song-form" data-id="{{$song->id}}">
-                    @csrf
-                    <div class="favorite-element">
-                        <div class="favorite-cover">
-                            <img src="{{$song->album_cover}}">
-                        </div>
-                        <div class="favorite-info">
-                            <div class="favorite-title">
-                                <span>{{ $song->name }}</span>
-                                <p><a href="{{ route('artist.show', $song->artist_slug) }}">{{ $song->artist_name }}</a> •
-                                    <a href="{{ route('album.show', $song->album_slug) }}">{{ $song->album_name }}</a></p>
-                            </div>
-                        </div>
-                        <div class="favorite-delete">
-                            {{-- <form action="{{ route('favorite.store') }}" class="actionFavorite" method="post"> --}}
-                                {{-- {{ csrf_field() }} --}}
-                                <input name="title" type="hidden" value="{{ $song->id }}">
-                                <input name="csrf" type="hidden" value="{{ csrf_token() }}">
-                                {{-- @csrf --}}
-                                <button type="button" id="favoriteButton" class="favorite-button" data-id="{{ $song->id }}">
-                                    <img src="{{ URL::to('/img') }}/close.svg" alt="">
-                                </button>
-                            {{-- </form> --}}
-
-                        </div>
-                        <input type="submit" class="play-song-submit" value="">
-                        <input type="hidden" name="song_id" value="{{ $song->id }}">
-                        <input type="hidden" name="position" value="{{$i}}">
-                    </div>
-
-                </form>
-                @php
-                    $i++;
-                @endphp
-                @endforeach
+            <div class="no-favorite-content">
+                <p class="message">
+                    Vous n'avez aucun titre dans vos favoris...<br>Qu'attentez-vous?
+                </p>
             </div>
+        @else
+            <span>Durée : {{ $length }}</span>
         </div>
+        <div class="favorite-actions">
+            <form action="{{ route('play.fastplayfavorite') }}" method="post" id="playFavoriteBtn">
+                @csrf
+                <input type="hidden" value="1" name="position">
+                <button type="submit" class="play-playlist"><img src="{{ URL::to('/img') }}/play.svg"
+                        alt=""><span>Lire</span></button>
+
+            </form>
+            <form action="{{ route('play.randomplayfavorite') }}" method="post" id="playRandomFavorite">
+                @csrf
+                <button type="submit" id="playPlaylistRandom" class="play-playlist-random"><img
+                        src="{{ URL::to('/img') }}/randomizer.svg" alt=""><span>Aléatoire</span></button>
+            </form>
+        </div>
+    </div>
+    </div>
+
+    <div class="favorite-playlist">
+        @php
+            $i = 1;
+        @endphp
+        @foreach ($songs as $song)
+            <form action="{{ route('song.uniqueplay') }}" method="post" class="favorite-unique-song-form"
+                data-id="{{ $song->id }}">
+                @csrf
+                <div class="favorite-element">
+                    <div class="favorite-cover">
+                        <img src="{{ $song->album_cover }}">
+                    </div>
+                    <div class="favorite-info">
+                        <div class="favorite-title">
+                            <span>{{ $song->name }}</span>
+                            <p><a href="{{ route('artist.show', $song->artist_slug) }}">{{ $song->artist_name }}</a> •
+                                <a href="{{ route('album.show', $song->album_slug) }}">{{ $song->album_name }}</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="favorite-delete">
+                        {{-- <form action="{{ route('favorite.store') }}" class="actionFavorite" method="post"> --}}
+                        {{-- {{ csrf_field() }} --}}
+                        <input name="title" type="hidden" value="{{ $song->id }}">
+                        <input name="csrf" type="hidden" value="{{ csrf_token() }}">
+                        {{-- @csrf --}}
+                        <button type="button" id="favoriteButton" class="favorite-button" data-id="{{ $song->id }}">
+                            <img src="{{ URL::to('/img') }}/close.svg" alt="">
+                        </button>
+                        {{-- </form> --}}
+
+                    </div>
+                    <input type="submit" class="play-song-submit" value="">
+                    <input type="hidden" name="song_id" value="{{ $song->id }}">
+                    <input type="hidden" name="position" value="{{ $i }}">
+                </div>
+
+            </form>
+            @php
+                $i++;
+            @endphp
+        @endforeach
+    </div>
+    @endif
+    </div>
     </div>
 @endsection
